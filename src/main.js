a1b982aed4b8a7ff683e88f69b07a47b99e4ed4b
narney <709447790@qq.com>
2019-03-25 21:51:36 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -3,25 +3,32 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from './routers/index'
 
+// 引入ajax模块
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
 
+// 引入UI库
 import Iview from 'iview'
 import '../statics/theme.less'
 
 Vue.config.productionTip = false
+// 加载路由
 Vue.use(VueRouter)
+// 加载UI库
 Vue.use(Iview)
 
+// ajax配置文件
 const axios = Axios.create({
   baseURL: '/api',
   methods: 'post'
 })
+// ajax返回体封装
 axios.interceptors.response.use(function(response){
   return response.data
 }, function(error) {
   return Promise.reject(error)
 })
+// 载入axios
 Vue.use(VueAxios, axios)
 
 new Vue({
